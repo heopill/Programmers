88a4287033822f061218907b4a8bdb6eef23adc0
SeongPil Heo <heopill@naver.com>
2025-03-18 10:17:00 +0900

    [level 0] Title: 머쓱이보다 키 큰 사람, Time: 0.05 ms, Memory: 16.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120585. 머쓱이보다 키 큰 사람/머쓱이보다 키 큰 사람.swift
@@ -0,0 +1,11 @@
+import Foundation
+
+func solution(_ array:[Int], _ height:Int) -> Int {
+    var result = 0
+    for i in array {
+        if i > height {
+            result += 1
+        }
+    }
+    return result
+}
