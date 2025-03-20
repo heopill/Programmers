17d9e3389895fed3409e020dcb8fbbaf6c6c103b
SeongPil Heo <heopill@naver.com>
2025-03-20 09:26:30 +0900

    [level 0] Title: 양꼬치, Time: 0.01 ms, Memory: 16.5 MB -BaekjoonHub

--- 프로그래머스/0/120830. 양꼬치/양꼬치.swift
+++ 프로그래머스/0/120830. 양꼬치/양꼬치.swift
@@ -1,12 +1,5 @@
 import Foundation
 
 func solution(_ n:Int, _ k:Int) -> Int {
-    var result = 0
-    if n >= 10 {
-        var mok = n / 10
-        result = (n * 12000) + (k * 2000) - (mok * 2000)
-    } else {
-        result = (n * 12000) + (k * 2000)
-    }
-    return result
+    return (n * 12000) + (k * 2000) - (n/10 * 2000)
 }
