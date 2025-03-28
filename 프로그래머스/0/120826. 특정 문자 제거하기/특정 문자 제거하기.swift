0b8830c92a40f75293c0e3606bffec1eb0a1d44d
SeongPil Heo <heopill@naver.com>
2025-03-28 12:32:52 +0900

    [level 0] Title: 특정 문자 제거하기, Time: 0.15 ms, Memory: 16.3 MB -BaekjoonHub

--- 프로그래머스/0/120826. 특정 문자 제거하기/특정 문자 제거하기.swift
+++ 프로그래머스/0/120826. 특정 문자 제거하기/특정 문자 제거하기.swift
@@ -1,6 +1,6 @@
 import Foundation
 
 func solution(_ my_string:String, _ letter:String) -> String {
-    guard my_string.count >= 1 && my_string.count <= 1000 else { return "" }
+    guard my_string.count >= 1 && my_string.count <= 100 else { return "" }
     return my_string.components(separatedBy: letter).joined()
 }
