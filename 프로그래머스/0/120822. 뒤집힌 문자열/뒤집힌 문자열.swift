b1075144262f8b4860e6158290d0ae2f14a854a1
SeongPil Heo <heopill@naver.com>
2025-04-01 09:50:43 +0900

    [level 0] Title: 뒤집힌 문자열, Time: 0.06 ms, Memory: 16.3 MB -BaekjoonHub

--- 프로그래머스/0/120822. 뒤집힌 문자열/뒤집힌 문자열.swift
+++ 프로그래머스/0/120822. 뒤집힌 문자열/뒤집힌 문자열.swift
@@ -1,6 +1,20 @@
 import Foundation
 
-func solution(_ my_string:String) -> String {
+func solution(_ my_string: String) -> String {
     guard my_string.count >= 1 && my_string.count <= 1000 else { return "" }
-    return String(my_string.reversed())
+
+    var array: [String] = []
+    var reverseArray: [String] = []
+    
+    // 문자열을 배열로 변환
+    for i in my_string {
+        array.append(String(i))
+    }
+    
+    // 배열을 거꾸로 순회하며 새로운 배열에 추가
+    for j in stride(from: array.count - 1, through: 0, by: -1) {
+        reverseArray.append(array[j])
+    }
+
+    return reverseArray.joined()
 }
