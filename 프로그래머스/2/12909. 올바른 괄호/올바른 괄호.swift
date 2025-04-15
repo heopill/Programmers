a6957e9100536c02af2c7ba4c834b8120042dc76
SeongPil Heo <heopill@naver.com>
2025-04-15 09:46:16 +0900

    [level 2] Title: 올바른 괄호, Time: 6.34 ms, Memory: 16.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.swift
@@ -0,0 +1,35 @@
+import Foundation
+
+func solution(_ s:String) -> Bool
+{
+    guard s.count <= 100_000 else { return false}
+    var ans:Bool = false
+    var stack: Int = 0
+    
+    // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+    print("Hello Swift")
+    
+    if s.prefix(1) == ")" {  // 시작이 닫힌 괄호면 false 출력
+        return false
+    } else { // 열린 괄호로 시작하면 for문 수행
+        for i in s {
+            if i == "(" {
+                stack += 1
+            } else if i == ")" {
+                stack -= 1
+            }
+            
+            if stack < 0 {
+                return false
+            }
+        }
+    }
+    
+    if stack == 0 { // 스택 카운트가 0이면 올바른 괄호
+        ans = true
+    } else { // 스택 카운트가 0이 아니면 올바르지 않은 괄호
+        ans = false
+    }
+    
+    return ans
+}
