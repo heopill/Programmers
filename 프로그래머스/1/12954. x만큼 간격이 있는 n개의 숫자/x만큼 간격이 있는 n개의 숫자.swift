ba7c9b449093f0609cabd86eac8a79a399dd0a3f
SeongPil Heo <heopill@naver.com>
2025-07-10 19:58:24 +0900

    [level 1] Title: x만큼 간격이 있는 n개의 숫자, Time: 0.10 ms, Memory: 16.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/12954. x만큼 간격이 있는 n개의 숫자/x만큼 간격이 있는 n개의 숫자.swift
@@ -0,0 +1,11 @@
+func solution(_ x:Int, _ n:Int) -> [Int64] {
+    var result = [Int64]()
+    var appendNum = x
+    
+    for i in 1...n {
+        result.append(Int64(appendNum))
+        appendNum += x
+    }
+    
+    return result
+}
