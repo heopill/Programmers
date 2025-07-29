b94593248922cd48bb877dd8b403eea71019f2bd
SeongPil Heo <heopill@naver.com>
2025-07-29 19:54:26 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.02 ms, Memory: 16.3 MB -BaekjoonHub

--- 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.swift
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.swift
@@ -3,21 +3,17 @@ func solution(_ num:Int) -> Int {
     var collatzNum = num
     
     while (collatzNum != 1) {
-        if num == 1 {
-            count = 0
-            break
-        }
-        if count == 500 {
+        if count >= 500 {
             count = -1
             break
         }
         if collatzNum % 2 == 0 {
             collatzNum = collatzNum / 2
-            count += 1
         } else {
             collatzNum = collatzNum * 3 + 1
-            count += 1
         }
+        count += 1
     }
     return count
 }
+
