1105c31a64dc92738b67398b1f1641efdb703e48
SeongPil Heo <heopill@naver.com>
2025-08-14 22:10:00 +0900

    [level 0] Title: 공배수, Time: 0.01 ms, Memory: 16.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181936. 공배수/공배수.swift
@@ -0,0 +1,10 @@
+import Foundation
+
+func solution(_ number:Int, _ n:Int, _ m:Int) -> Int {
+    if number % n == 0 {
+        if number % m == 0 {
+            return 1
+        }
+    }
+    return 0
+}
