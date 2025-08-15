948a926c5daadc79570c8162df493b30296a0ef7
SeongPil Heo <heopill@naver.com>
2025-08-16 02:54:37 +0900

    [level 0] Title: 원소들의 곱과 합, Time: 0.03 ms, Memory: 16.2 MB -BaekjoonHub

--- 프로그래머스/0/181929. 원소들의 곱과 합/원소들의 곱과 합.swift
+++ 프로그래머스/0/181929. 원소들의 곱과 합/원소들의 곱과 합.swift
@@ -4,10 +4,6 @@ func solution(_ num_list:[Int]) -> Int {
     var mul = num_list.reduce(1, *)
     var sum = num_list.reduce(0, +)
     
-    if mul < sum * sum {
-        return 1
-    } else {
-        return 0
-    }
+    return mul < sum * sum ? 1 : 0
 
 }
