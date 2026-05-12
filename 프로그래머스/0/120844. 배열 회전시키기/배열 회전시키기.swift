9639bbdd51a57fb2014e77dfe428efd03e48e2c5
SeongPil Heo <heopill@naver.com>
2026-05-12 20:08:18 +0900

    [level 0] Title: 배열 회전시키기, Time: 0.03 ms, Memory: 15.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/120844. 배열 회전시키기/배열 회전시키기.swift
@@ -0,0 +1,17 @@
+import Foundation
+
+func solution(_ numbers:[Int], _ direction:String) -> [Int] {
+    var result = numbers
+
+    if direction == "left" {
+        let first = result.removeFirst()
+        
+        result.append(first)
+    } else if direction == "right" {
+        let last = result.removeLast()
+
+        result.insert(last, at: 0)
+    }
+    
+    return result
+}
